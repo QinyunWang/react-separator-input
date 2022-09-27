d2ff9d8deb7a6dd14ce832d04dc537b4fb864b96
Qinyun Wang <oojbjoo@gmail.com>
2022-09-28 00:08:56 +0800

    feat: doc config

--- .umirc.ts
+++ .umirc.ts
@@ -2,16 +2,10 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'react-separator-input',
-  favicon:
-    process.env.NODE_ENV === 'production'
-      ? './react-separator-input/assets/ke.ico'
-      : '/assets/ke.ico',
-  logo:
-    process.env.NODE_ENV === 'production'
-      ? './react-separator-input/images/logo.svg'
-      : '/images/logo.svg',
+  favicon: process.env.NODE_ENV === 'production' ? './assets/ke.ico' : '/assets/ke.ico',
+  logo: process.env.NODE_ENV === 'production' ? './images/logo.svg' : '/images/logo.svg',
   outputPath: 'docs-dist',
   base: '/react-separator-input/',
-  publicPath: process.env.NODE_ENV === 'production' ? './react-separator-input/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   // more config: https://d.umijs.org/config
 });
